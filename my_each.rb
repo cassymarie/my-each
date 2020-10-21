6d6ae008d35d2a4b8d5087797e4f9eb1d0b96943
cassymarie <cassy2205@gmail.com>
2020-10-21 11:36:30 -0600

    commit tree 58c9ad2cd60ff74caafd36355156f979335911be

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
+collection = [1,2,3,4]
+
+def my_each(array)
+  x = 0
+  while x < array.length
+    yield (array[x])
+    x += 1
+  end
+  array
+end
+
+my_each(collection) do |name|
+  return "#{name}"
 end
